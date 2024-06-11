71fd0d4135aba04ed0859ed43acb54702a0b5d06
waleed-786 <121564794+waleed-786@users.noreply.github.com>
2024-06-11 13:59:29 +0500

    Update lab2q2.sv

--- lab2q2.sv
+++ lab2q2.sv
@@ -13,13 +13,7 @@ full adder (
 .a(al), .b(b1), .c(c1), .sum(sumi), .carry(carry1) );
 
 initial
-
 begin
-
-// Provide different combinations of the inputs to check validity of code a=0;b=0;c=0;
-
-
-
 a1=0;b1=0;c1=0;
  #10; 
  a1=0; b1=1; c1=0;
@@ -32,8 +26,6 @@ a1=0;b1=0;c1=0;
        a1=1;b1=1;c1=0 ;
         #10; a1=1;b1=1;c1=1;
          #10;
-
 $stop;
 end
-
 endmodule
